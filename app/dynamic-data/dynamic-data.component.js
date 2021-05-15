ea8a1f30960ae7ebf0b08b347c793dc3933eeabc
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2021-05-15 15:55:34 +0100

    Component controller behaviour to update state

--- app/dynamic-data/dynamic-data.component.js
+++ app/dynamic-data/dynamic-data.component.js
@@ -5,4 +5,28 @@ angular.module("dynamicData").component("dynamicData", {
         data: "<",
         response: "<",
     },
+    // Inject a service from this module into the component controller
+    controller: function DynamicDataController(dynamicDataService) {
+        var ctrl = this;
+
+        // State
+        ctrl.dynamicData = {};
+        ctrl.dataMessage = "";
+        ctrl.clickCount = 0;
+
+        // Behaviour
+        ctrl.updateDynamicData = function () {
+            dynamicDataService.getData().then(function (response) {
+                console.log(response);
+                ctrl.dynamicData = response;
+            });
+
+            ctrl.updateDataMessage();
+        };
+
+        ctrl.updateDataMessage = function () {
+            ctrl.clickCount++;
+            ctrl.dataMessage = "Updated for the " + ctrl.clickCount + " time";
+        };
+    },
 });
